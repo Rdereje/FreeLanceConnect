cb64636ecf7da2778585c7f8fdf1418e97e8e53c
Rdereje <rud6@pitt.edu>
2019-01-25 20:31:41 -0500

    add method

--- FreeLance.java
+++ FreeLance.java
@@ -1,9 +1,17 @@
 import java.util.*;
 public class FreeLance
 {
+	List<Person> Info;
+	int IDcount;
 	FreeLance()
 	{
-		
+		Info = new ArrayList<Person>();
+		IDcount = 0;
+	}
+	void Add(String name, String password)
+	{
+		Info.add(new Person(IDcount, name, password));
+		IDcount++;
 	}
 	class Person
 	{
@@ -12,12 +20,15 @@ public class FreeLance
 		String password;
 		int booking;
 		int earned;
-		ArrayList<String> tags = new ArrayList<String>();
+		ArrayList<String> tags;
 		Person(int ID, String name, String password)
 		{
 			this.ID = ID;
 			this.name = name;
 			this.password = password;
+			booking = 0;
+			earned = 0;
+			tags = new ArrayList<String>();
 		}
 	}
 }
